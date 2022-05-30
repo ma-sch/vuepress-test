46c939e29e2ee34a8c7a566bc6bacab1b10998cc
mat30050 <matthias.schneider@ipa.fraunhofer.de>
2022-05-30 13:20:44 +0200

    Initial commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'TestDocu',
+    description: 'Test Documentation',
+    themeConfig: {
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Guide', link: '/guide/' },
+            { text: 'About me', link: 'https://google.com' },
+        ]
+    }
+}
---
+++ docs/README.md
@@ -0,0 +1,4 @@
+#Hello VuePoress 
+
+
+ASdASd
